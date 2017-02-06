6aa21ef44bb4fb1cf629c1ea39ea454094a0bd75
SyedAbuthahirms <abuazeem2014@gmail.com>
2017-02-06 14:55:06 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,215 @@
+<html> 
+<title></title> 
+<head> 
+<link href='http://fonts.gooleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'> 
+<link href='http://fonts.gooleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'> 
+<style type='text/css'> 
+body{
+margin:0px;
+padding:0px;
+}
+.wrapper{ 
+width:420px; 
+margin:auto; 
+} 
+p{ 
+font:13px; 
+color:#000000; 
+text-align:center;
+} 
+.form{ 
+width:100%; 
+} 
+input[type='text'],input[type='email']{ 
+width:98%; 
+padding:15px 0px 15px 8px; 
+border-radius:5px; 
+border:1px solid rgba(0,0,0,1); 
+margin-bottom:10px; 
+color:#6E6E6E; 
+text-shadow:#000; 
+} 
+input[type='submit']{ 
+width:100%; 
+padding:15px; 
+border-radius:5px; 
+@include disable; 
+background-image:-webkit-gradient(linear,0,0,0 100%, from(#28D2DE), to(#1A878F)); 
+background-image:-webkit-linear-gradient(#28D2DE 0%, #1A878F 100%); 
+background-image:-moz-linear-gradient(#28D2DE 0%, #1A878F 100%); 
+background-image:-o-linear-gradient(#28D2DE 0%, #1A878F 100%); 
+background-image:linear-gradient(#28D2DE 0%, #1A878F 100%); 
+font:14px Oswald; 
+color:#fff; 
+text-transformLuppercase; 
+box-shadow:#fff 0px 1px 5px; 
+border:1px solid:#000; 
+opacity:0.7; 
+
+} 
+input:focus{ 
+box-shadow:inset 4px 6px 10px -4px rgba(0,0,0,0.7), 0 1px 1px -1px rgba(255,255,255,0.3); 
+@include easeme; 
+} 
+input[type='submit']:hover{ 
+opacity:1; 
+cursor:pointer; 
+} 
+.footer-top{
+background:#f8f8f8;
+margin-left:35%;
+
+}
+.footer-top h3 {
+  margin: 0 0 20px 0;
+  font-size: 22px;
+  font-family: 'Droid Serif', serif;
+  color: #26c7db;
+
+}
+.footer-grid ul{
+margin:0;
+padding:0;
+
+}
+.footer-grid ul li{
+padding-left:30px;
+list-style-type:none;
+line-height:2em;
+display:inline;
+}
+.footer-grid ul li a{
+color:#000000;
+font-size:14px;
+text-decoration:none;
+v
+}
+.footer-grid ul li a:hover{
+  color: #6cd79c;
+}
+.speaker-inline
+{
+list-style-type:none;
+padding-lefy: 12px;
+}
+.speaker-inline li{
+display:inline;
+padding : 24px;
+text-align: center;
+}
+.speaker-inline li a{
+color:#000000;
+font-size:14px;
+text-decoration:none;
+}
+.speaker-inline img{
+border-radius:10%;
+width:20px;
+height:20px;
+}
+ .col-md-3 {
+    float: left;
+}
+h4 a
+{
+text-decoration:none;
+cursor:pointer;
+}
+</style> 
+</head> 
+<body bgcolor="skyblue"> 
+<h4 align="center"><i><a href="https:\\www.youtube.com" target="_blank">Live/Interactive</a></i></h4>
+<hr />
+<br />
+<div class='wrapper'> 
+<h4 align="center"><i>Register for Free</i></h4>
+<form class='form' method='post' action=''> 
+<input type='text' name='name' class='name' placeholder='Name' /> 
+
+<input type='text' name='mobilenumber' class='mobilenumber' placeholder='Mobile number' /> 
+
+<input type='text' name='email' placeholder='email' /> 
+
+<input type='submit' class='submit' name="submit" value='Register'> 
+        </form> 
+
+</div>
+<hr />
+<h4 align="center"><i>Speakers</i></h4>
+                        <ul class="speaker-inline">
+                            <li><img src="https://image.freepik.com/free-icon/male-profile-user-shadow_318-40244.jpg"  /><a href="https://www.linkedin.com/" 
+
+target="_blank">Manvender Singh</a></li>
+                            <li><img src="https://image.freepik.com/free-icon/male-profile-user-shadow_318-40244.jpg"  /><a href="https://www.linkedin.com/" 
+
+target="_blank">Sandeep Tompala</a></li>
+                            <li><img src="https://image.freepik.com/free-icon/male-profile-user-shadow_318-40244.jpg"  /><a href="https://www.linkedin.com/" 
+
+target="_blank">Manvender Singh</a></li>
+                            <li><img src="https://image.freepik.com/free-icon/male-profile-user-shadow_318-40244.jpg"  /><a href="https://www.linkedin.com/" 
+
+target="_blank">Mandever Singh</a></li>
+                            <li><img src="https://image.freepik.com/free-icon/male-profile-user-shadow_318-40244.jpg"  /><a href="https://www.linkedin.com/" 
+
+target="_blank">Sandeep Tompala</a></li>
+                        </ul> 
+<hr />
+<h4 align="center"><i>About Us</i></h4>
+<p>
+In this digital era, the right data tools have the power to transform businesses by giving them a definitive edge. Powered and promoted by Tech Mahindra,
+
+one of the foremost names in Information Technology today, UpX is a world class education venture for Big Data professionals. We recognize both the
+
+challenge and opportunity, and are committed to educating and empowering professionals through transformational courses.
+</p>
+<hr />
+<h4 align="center"><i>Our Courses</i></h4>
+<div class="footer-top">
+	<div class="container">
+		<div class="footer-grids">
+			<div class="col-md-3 footer-grid">
+			<h3>Big Data</h3>
+			<ul>
+			<li><a href="https://upxacademy.com/" target="_blank">Hadoop</a></li><br />
+			<li><a href="https://upxacademy.com/" target="_blank">Apache Spark</a></li><br />
+			<li><a href="https://upxacademy.com/" target="_blank">Mongo</a></li><br />
+			</ul>
+			</div>
+			<div class="col-md-3 footer-grid">
+			<h3>Data Science</h3>
+			<ul>
+			<li><a href="https://upxacademy.com/ " target="_blank">Data analytics</a></li><br />
+			<li><a href="https://upxacademy.com/">Machine Learning</a></li><br />
+			<li><a href="https://upxacademy.com/" target="_blank">Tableau</a></li><br />
+			</ul>
+			</div>
+			<div class="col-md-3 footer-grid">
+			<h3>To Know More</h3>
+			<ul>
+			<li><a href="https://upxacademy.com/" target="_blank">Click Here</a></li>
+			</ul>	
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+<?php
+$con=mysql_connect("localhost","root","");
+mysql_select_db("techie",$con);
+if(isset($_POST['submit']))
+{
+$name=$_POST["name"];
+$mobile=$_POST["mobilenumber"];
+$email=$_POST["email"];
+
+if(mysql_query("INSERT INTO tech(username,mobilenumber,email)   VALUES('$name','$mobile','$email')"))
+{
+echo "resgistered successfully";
+}
+else{
+echo "cannot register now......try again later";
+}
+}
+?>
+</body> 
+</html>
